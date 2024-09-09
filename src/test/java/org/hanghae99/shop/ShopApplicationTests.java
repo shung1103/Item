9afd4c3337f54840d5a5fb63737e7c8e6c679a45
shung1103 <shung1103@hanmail.net>
2024-09-09 10:06:39 +0900

    start

---
+++ src/test/java/org/hanghae99/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hanghae99.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
